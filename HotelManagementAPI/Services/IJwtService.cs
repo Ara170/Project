d7206207f1fe97b797397e2d3bd7757cc474ce97
Ara170 <manh.thai.170@gmail.com>
2025-05-06 15:00:25 +0700

    Delete HotelManagementAPI directory

--- HotelManagementAPI/Services/IJwtService.cs
+++
@@ -1,9 +0,0 @@
-using HotelManagementAPI.Models;
-
-namespace HotelManagementAPI.Services
-{
-    public interface IJwtService
-    {
-        string GenerateJwtToken(User user, string roleName);
-    }
-}
--- HotelManagementAPI/Services/IPasswordService.cs
+++
@@ -1,8 +0,0 @@
-namespace HotelManagementAPI.Services
-{
-    public interface IPasswordService
-    {
-        (byte[] passwordHash, string salt) HashPassword(string password);
-        bool VerifyPassword(string password, byte[] storedHash, string salt);
-    }
-}
